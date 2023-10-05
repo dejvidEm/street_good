814bb71a8bba64dea9915c1c20a32cfda65642b4
dejvidEm <bithug10@gmail.com>
2023-10-05 22:25:55 +0200

    L

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -30,7 +30,7 @@ export default function Intro() {
                 <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
                 </svg>
             </div>
-        <section className="text-center bg-pink-400 w-full h-auto">
+        <section className="text-center bg-pink-400 w-full">
           <div className={`loader${isLoading ? '' : ' loader--hidden'}`}></div>
             <div className="h-screen translate-y-1/3 z-10">
                 <h1 className="sm:text-6xl text-4xl font-semibold text-white">STREET GOOD</h1>
