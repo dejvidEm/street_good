d2284c1fb7e00c478529259e98f172816f636f60
dejvidEm <bithug10@gmail.com>
2023-10-05 19:58:39 +0200

    update

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -8,15 +8,9 @@ export default function Intro() {
   useEffect(() => {
     const loader = document.querySelector(".loader");
 
-    const handleTransitionEnd = () => {
-      if (loader && document.body.contains(loader)) {
-        
-      }
-    };
 
     if (loader instanceof HTMLElement) {
       loader.classList.add("loader--hidden");
-      loader.addEventListener("transitionend", handleTransitionEnd);
     }
 
     // Simulujeme načítanie po 3 sekundách
@@ -26,15 +20,12 @@ export default function Intro() {
 
     return () => {
       clearTimeout(timer);
-      if (loader instanceof HTMLElement) {
-        loader.removeEventListener("transitionend", handleTransitionEnd);
-      }
     };
   }, []);
 
     return (
         <section className="text-center bg-pink-400 w-full h-screen">
-            <div className={`loader${isLoading ? '' : ' loader--hidden'}`}></div>
+          <div className={`loader${isLoading ? '' : ' loader--hidden'}`}></div>
             <div className="h-screen translate-y-1/3 z-10">
                 <h1 className="sm:text-6xl text-4xl font-semibold text-white">STREET GOOD</h1>
                 <h2 className="text-white italic font-light text-2xl p-5">Time for the smoothie</h2>
@@ -42,8 +33,8 @@ export default function Intro() {
             <div className="wave">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                 <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
-    </svg>
-</div>
+                </svg>
+            </div>
         </section>
     )
 }
